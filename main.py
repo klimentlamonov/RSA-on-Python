473930175adbc4fc82b4acd197989aa699606b47
klimentlamonov <1_0_91@bk.ru>
2020-10-07 17:01:11 +0300

    redisingn code and interface

--- main.py
+++ main.py
@@ -8,8 +8,8 @@
 
 from PySide2 import QtWidgets
 from main_ui import Ui_Form
-import sys
 from random import randint
+import sys
 
 # create application
 app = QtWidgets.QApplication(sys.argv)
@@ -20,205 +20,141 @@ ui = Ui_Form()
 ui.setupUi(Form)
 Form.show()
 
-# logic part
-gretings = "##########################\n##   Make RSA Protocol  ##\n##     Using Python     ##\n##                      ##\n##       Made by        ##\n##     Lamonov K.A.     ##\n##        2020          ##\n##########################"
-togo = "Чтобы запустить программу введите сообщение в поле 'Отправитель А', введите p и q, затем нажмите кнопку [Начать].\n"
-error = "########ERROR########\nИсправте ошибку и нажмите кнопку [Начать]\n"
+# logic
+GREETINGS = "##########################\n##   Make RSA Protocol  ##\n##     Using Python     ##\n##                      ##\n##       Made by        ##\n##     Lamonov K.A.     ##\n##        2020          ##\n##########################"
+TOGO = "Чтобы запустить программу введите сообщение в поле 'Отправитель А', затем нажмите кнопку [START].\n"
+ERROR = "########ERROR########\nИсправте ошибку и нажмите кнопку [Начать]\n"
 
-def printConsol(string):
-	ui.plainTextEdit_3.appendPlainText( string )
+class RSA:
+	def __init__(self, *pq):  # konstructor 
+		self.p = pq[0][0]
+		self.q = pq[0][1]
+	
+	def start(self):  # firs step making n, eler-func, e & d
+		self.n = self.p * self.q
+		self.eler = (self.p - 1) * (self.p - 1)
+		while True:
+			self.e = randint(1, self.eler)
+			if nod(self.e, self.eler) == 1: break
+		self.d = 0
+		while (self.d * self.e) % self.eler != 1:
+			self.d += 1
 
-def myHash(text):
-	i = 0
-	myHash = 0
-	lenght = len(text)
-	while i < lenght:
-		myHash += lenght + i
-		i += 1
-	return myHash
+class DigitalSignature:
+	def __init__(self, text, d, n):
+		self.n = n
+		self.h = myHash(text)
+		self.s = (self.h ** d) % self.n
+	
+	def check(self, text, e):
+		self._h = myHash(text)
+		self.h = (self.s ** e) % self.n
+		
 
-def checkPrime(p, q): # проверка на простоту 1-ый способ
-	flag = 0
-	i = 2
-	while i <= int(p ** 0.5):
-	    if p % i == 0:
-	       	flag += 1
-	       	break
-	    i += 1
 
-	i = 2
-	while i <= int(q ** 0.5):
-	    if q % i == 0:
-	       	flag += 2
-	       	break
-	    i += 1
+def printConsol(string):  # print in plaintext
+	ui.plainTextEdit.appendPlainText(string)
 
-	return flag
+def checkPrime(digital):  # check digital for prime
+	for i in range(2, int(digital ** 0.5) - 1):
+		if digital % i == 0: return False
+	return True
 
-def nod(f, s):
-	i = 1
-	lst = []
-	if f > s:
-		min = s
-	elif f <= s:
-		min = f
-
-	while i <= min:
-		if f % i == 0 and s % i == 0:
-			lst.append(i)
-		i += 1
-	return lst[-1]
-
-def findD(e, fe):
-	d = 0
-	while (d * e) % fe != 1:
-		d += 1
-	return d
-
-def createPQ():
-	lst = []
-	for num in range(2, 201):
-		if checkPrime(num, 4) == 2:
-			lst.append(num)
-
-	flag = 0
-	while flag != 2:
-		i = randint(0, len(lst) - 1)
-		if flag == 0:
-			p = lst[i]
-		elif flag == 1:
-			q = lst[i]
-		flag += 1
+def createPQ():  # create random prime p & q
+	lst = [num for num in range(2, 201) if checkPrime(num)]
+	p = lst[randint(0, len(lst) - 1)]
+	q = lst[randint(0, len(lst) - 1)]
 	return p, q
 
-def startRSA():
-	p, q = createPQ()
-	ui.lineEdit.insert(str(p))
-	ui.lineEdit_2.insert(str(q))
-	if not(str(p).isdigit() and str(q).isdigit()):
-		printConsol( "Ошибка. В поля для p и q введите целые числа!" )
-		printConsol( error )
+def nod(f, s):  # NOD
+	lst = []
+	if f > s:
+		_min = s 
 	else:
-		printConsol( "Введены числа\np = " + str(p))
-		printConsol( "q = " + str(q))
-		printConsol( "~~~~~~~~~~~~~~~~~~~~~~~" )
+		_min = f
+	for i in range(1, _min + 1):
+		if f % i == 0 and s % i == 0:
+			lst.append(i)
+	return lst[-1]
 
-		printConsol( "Выполним проверку на простоту чисел..." )
-		p = int(p)
-		q = int(q)
-		flagPrime = False
-		if checkPrime(p, q) == 3:
-		    printConsol( "Проверка: Провал. Оба числа не являются простыми" )
-		elif checkPrime(p, q) == 1:
-		    printConsol( "Проерка: Провал. Число p не является простым" )
-		elif checkPrime(p, q) == 2:
-		    printConsol( "Проерка: Провал. Число q не является простым" )
-		elif checkPrime(p, q) == 0:
-		    printConsol("Проерка: Успех. Оба числа являются простыми")
-		    flagPrime = True
-		printConsol( "~~~~~~~~~~~~~~~~~~~~~~~" )
+def myHash(text):
+	myHash = 0
+	for i in range(len(text)):
+		myHash += len(text) + i
+	return myHash
 
-		if flagPrime == False:
-			printConsol( "Необходимо, чтобы числа p и q были простыми.\n" )
-			printConsol( error )
-		else:
-			# first step making keys by RSA algoritm 
-			printConsol( "ЭТАП 1. Генерируем ключи по алгоритму RSA.\n" )
-			printConsol( "1.1 Вычисляем n" )
-			n = p * q
-			printConsol( "n = " + str(n) + "\n")
+def bp():  # button logic
+	ui.plainTextEdit.setPlainText(GREETINGS)
+	ui.plainTextEdit_3.setPlainText("Получатель B")
 
-			printConsol( "1.2 Вычисляем функцию Эйлера" )
-			fe = (p - 1)*(q - 1)
-			printConsol( "fe(n) = " + str(fe) + "\n")
+	x = RSA(createPQ())
 
-			printConsol( "1.3 Выбираем открый ключ e. Взаимно простой с результатом функции Эйлера" )
-			while True:
-				e = randint(1, fe)
-				if nod(e, fe) == 1:
-					printConsol( "e = " + str(e) + "\n" )
-					break
+	printConsol( "Числа\np = " + str(x.p))
+	printConsol( "q = " + str(x.q))
+	printConsol( "~~~~~~~~~~~~~~~~~~~~~~~" )
 
-			printConsol( "1.4 Вычисляем закрытый ключ d." )
-			d = findD(e, fe)
-			printConsol( "d = " + str(d) + "\n")
+	x.start()
 
-			printConsol( "Успех. Ключи сгенерированы." )
-			printConsol( "~~~~~~~~~~~~~~~~~~~~~~~" )
+	printConsol( "ЭТАП 1. Генерируем ключи по алгоритму RSA.\n" )
+	printConsol( "1.1 Вычисляем n" )
+	printConsol( "n = " + str(x.n) + "\n")
 
-			return n, d, e
+	printConsol( "1.2 Вычисляем функцию Эйлера" )
+	printConsol( "fe(n) = " + str(x.eler) + "\n")
 
-def textMessage(n,d):
+	printConsol( "1.3 Выбираем открый ключ e. Взаимно простой с результатом функции Эйлера" )
+	printConsol( "e = " + str(x.e) + "\n" )
+
+	printConsol( "1.4 Вычисляем закрытый ключ d." )
+	printConsol( "d = " + str(x.d) + "\n")
+
+	printConsol( "Успех. Ключи сгенерированы." )
+	printConsol( "~~~~~~~~~~~~~~~~~~~~~~~" )
+
 	printConsol( "ЭТАП 2. Отправка сообщений и Электронной подписи\n" )
 	printConsol( "2.1 Вычисление хеш-образа h = h(T), где T - исходное сообщение\n" )
-	text = ui.plainTextEdit.toPlainText()
-	if text == "":
-		printConsol("\nОшибка. В поле 'Отравитель А' отсутствует текст сообщения.\n")
-		printConsol( error )
-	else:
-		h = myHash(text)
-		printConsol( "h = " + str(h) + "\n" )
 
-		printConsol( "2.2 Выроботка цифровой подписи s" )
-		s = (h ** d) % n
-		printConsol( "s = " + str(s) + "\n" )
+	y = DigitalSignature(ui.plainTextEdit_2.toPlainText(), x.d, x.n)
 
-		printConsol( "Успех. Элктронная подпись создана." )
-		printConsol( "~~~~~~~~~~~~~~~~~~~~~~~" )
-		ui.plainTextEdit.appendPlainText( "\n#########\nХэш-сумма сообщения h(T) = " + str(h) )
+	printConsol( "h = " + str(y.h) + "\n" )
 
-		return text, s
+	printConsol( "2.2 Выроботка цифровой подписи s" )
+	printConsol( "s = " + str(y.s) + "\n" )
 
-def checkMessage(text, e, n, s):
+	printConsol( "Успех. Электронная подпись создана." )
+	printConsol( "~~~~~~~~~~~~~~~~~~~~~~~" )
+
 	printConsol( "ЭТАП 3. Получения сообщения и проверка электронной подписи.\n" )
 	printConsol( "3.1 Вычисление хеш образа по полученному сообщению h' = h(T')." )
-	h_ = myHash(text)
-	ui.plainTextEdit_2.appendPlainText( text + "\n\n#########\nХэш-сумма сообещния h'(T') = " + str(h_))
-	printConsol( "h' = " + str(h_) + "\n")
+
+	ui.plainTextEdit_3.setPlainText(ui.plainTextEdit_2.toPlainText())
+	ui.plainTextEdit_2.appendPlainText( "\n#########\nХэш-сумма сообщения h(T) = " + str(y.h))
+	y.check(ui.plainTextEdit_3.toPlainText(), x.e)
+
+	ui.plainTextEdit_3.appendPlainText("\n\n#########\nХэш-сумма сообещния h'(T') = " + str(y._h))
+	printConsol( "h' = " + str(y._h) + "\n")
 
 	printConsol( "3.2 Вычисление хеш-образа из цифровой подписи." )
-	h = (s ** e) % n
-	printConsol( "h = " + str(h) + "\n")
+	printConsol( "h = " + str(y.h) + "\n")
 
 	printConsol( "3.3 Сравнение h' и h." )
-	if h == h_:
+	if y.h == y._h:
 		printConsol( "Успех. Сообщение T действительно отправлено А." )
 	else:
 		printConsol( "Провал. Полученное сообщения не было отправелно А." )
 
+		
+	# button actions
+ui.pushButton.clicked.connect(bp)
 
 
+	# actions
+ui.plainTextEdit.setPlainText(GREETINGS)
+ui.plainTextEdit_2.setPlainText("Отправитель A")
+ui.plainTextEdit_3.setPlainText("Получатель B")
 
+ui.plainTextEdit.appendPlainText(TOGO)
 
-
-
-
-
-def bp_start(): # проверка простых чисел на простоту, генерация ключей и отправка их участинкам переписки 
-	printConsol( "Начинаем..." )
-	n, d, e = startRSA()
-
-	text, s = textMessage(n, d)
-
-	checkMessage(text, e, n, s)
-	
-
-
-def bp_refresh():
-	ui.plainTextEdit_3.setPlainText(gretings)
-	ui.plainTextEdit_3.appendPlainText(togo)
-	ui.plainTextEdit.setPlainText( "" )
-	ui.plainTextEdit_2.setPlainText( "" )
-	ui.lineEdit.clear()
-	ui.lineEdit_2.clear()
-
-
-
-ui.plainTextEdit_3.setPlainText(gretings)
-ui.plainTextEdit_3.appendPlainText(togo)
-
-ui.pushButton_3.clicked.connect( bp_start )
-ui.pushButton_4.clicked.connect( bp_refresh )
 
 
 # launch
